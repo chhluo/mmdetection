75ea7b14c3057d881f9ae0c6acf1cba1f76485bb
luochunhua <luochunhua1996@outlook.com>
2022-01-21 13:24:16 +0000

    update configs

--- configs/mask2former/mask2former_r50_lsj_8x2_50e_coco.py
+++ configs/mask2former/mask2former_r50_lsj_8x2_50e_coco.py
@@ -188,6 +188,7 @@ data = dict(
         ins_ann_file=data_root + 'annotations/instances_val2017.json',
     ))
 
+embed_multi = dict(lr_mult=1.0, decay_mult=0.0)
 # optimizer
 optimizer = dict(
     type='AdamW',
@@ -196,7 +197,13 @@ optimizer = dict(
     eps=1e-8,
     betas=(0.9, 0.999),
     paramwise_cfg=dict(
-        custom_keys={'backbone': dict(lr_mult=0.1, decay_mult=1.0)}))
+        custom_keys={
+            'backbone': dict(lr_mult=0.1, decay_mult=1.0),
+            'query_embed': embed_multi,
+            'query_feat': embed_multi,
+            'level_embed': embed_multi,
+        },
+        norm_decay_mult=0.0))
 optimizer_config = dict(grad_clip=dict(max_norm=0.01, norm_type=2))
 
 # learning policy
